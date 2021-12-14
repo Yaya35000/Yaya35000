64317eb905aa6477f70fc54b69e802ac0bb65944
Yaya35000 <94463311+Yaya35000@users.noreply.github.com>
2021-12-14 14:44:11 +0100

    Update projet.java

--- projetINF1/puissance4/src/puissance4/projet.java
+++ projetINF1/puissance4/src/puissance4/projet.java
@@ -4,19 +4,15 @@ import java.util.Scanner;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class projet {
-	public static int entierAleatoire(int a, int b){
-		//Retourne un entier aléatoire entre a (inclus) et b (inclus)
-		return ThreadLocalRandom.current().nextInt(a, b + 1);	
-	}
-	public static void afficheTab2D(int [][] tab ){
+	public static void afficheTab2D(int tab [][]) {
 		System.out.println("\n");
-        	for(int i=0;i<tab.length;i++) {
-            		for(int j=0;j<tab[i].length;j++) {
-                		System.out.print(tab[i][j]+" ");
-            		}
-            		System.out.println();
-        	}
-	}
+        for(int i=0;i<tab.length;i++) {
+            for(int j=0;j<tab[i].length;j++) {
+                System.out.print(tab[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
 	static int [][] grille;
 	public static int[][] initialisationGrille (){
 		grille = new int[6][7];
@@ -28,39 +24,43 @@ public class projet {
 		return grille;
 	}
 	public static void jouer (int joueur, int colonne) {
-		for(int i = 0; i<grille.length; i++) {
-			if(i==5) {
-				grille[i][colonne]=joueur;
-			}
-			else if (grille[i+1][colonne]!=0){
-				grille[i][colonne]=joueur;
-			}
-		}
-	}
+        boolean check=false;
+        for(int i = 0; i<grille.length; i++) {
+            if(i==grille.length-1 && !check) {
+                grille[grille.length-1][colonne]=joueur;
+                check = true;
+            }
+            else if (caseCorrecte(i, i+1) && !check){
+                if(grille[i+1][colonne]!=0) {
+                    grille[i][colonne]=joueur;
+                    check=true;
+                }
+            }
+        }
+    }
 	public static void afficheGrille() {
-		
 		for(int i=0;i<grille.length;i++) {
-			for(int j=0;j<grille[i].length;j++) {
-				if(j==0) {
-					if(grille[i][j]==0) System.out.print("|"+" "+"|");
-					else if(grille[i][j]==1) System.out.print("|"+"X"+"|");
-					else System.out.print("|"+"O"+"|");
-				}
-				else {
-					if(grille[i][j]==0) System.out.print(" "+"|");
-					else if(grille[i][j]==1) System.out.print("X"+"|");
-					else System.out.print("O"+"|");
-				}
-			}
-			System.out.println();
-		}
-		System.out.print("|");
-		for(int k=0;k<grille[0].length;k++) {
-			if(k<7) System.out.print(k +"|");
-		}
-	}
-	public static void main(String[] args) {
-
+            for(int j=0;j<grille[i].length;j++) {
+                if(j==0) {
+                    if(grille[i][j]==0) System.out.print("|"+" "+"|");
+                    else if(grille[i][j]==1) System.out.print("|"+"X"+"|");
+                    else System.out.print("|"+"O"+"|");
+                }
+                else {
+                    if(grille[i][j]==0) System.out.print(" "+"|");
+                    else if(grille[i][j]==1) System.out.print("X"+"|");
+                    else System.out.print("O"+"|");
+                }
+            }
+            System.out.println();
+        }
+        System.out.print("|");
+        for(int k=0;k<grille[0].length;k++) {
+            if(k<7) System.out.print(k +"|");
+        }
+    }
+	public static boolean caseCorrecte(int i,int j) {
+		return (grille.length>i && i>=0 && j>=0 && j<grille[0].length);
 	}
 	public static boolean aGagneHor (int x, int y, int joueur) {
         int compteur = 0;
@@ -98,41 +98,36 @@ public class projet {
         int a=y;
         if(!caseCorrecte(o, a)) return false;
         while(caseCorrecte(o, a)) {
-        	if(grille[o][a]==joueur) {
-        		compteur+=1;
-        		if(compteur==4) return true;
-        	}
-        	else compteur=0;
-        	o-=1;
-        	a+=1;
-        	
+            if(grille[o][a]==joueur) {
+                compteur+=1;
+                if(compteur==4) return true;
+            }
+            else compteur=0;
+            o-=1;
+            a+=1;
         }
         return false;
-        
     }
-	
 	public static boolean aGagneDiagDesc (int x, int y, int joueur) {
         int compteur = 0;
         int o=x;
         int a=y;
         if(!caseCorrecte(o, a)) return false;
         while(caseCorrecte(o, a)) {
-        	if(grille[o][a]==joueur) {
-        		compteur+=1;
-        		if(compteur==4) return true;
-        	}
-        	else compteur=0;
-        	o+=1;
-        	a+=1;
-        	
+            if(grille[o][a]==joueur) {
+                compteur+=1;
+                if(compteur==4) return true;
+            }
+            else compteur=0;
+            o+=1;
+            a+=1;
         }
         return false;
-        
     }
 	public static boolean aGagne(int joueur) {
 		for(int i=0;i<grille.length;i++) {
-			for(int j=0;i<grille[i].length;j++) {
-				if(aGagneHor(i, j, joueur)||aGagneVer(i, j, joueur)||aGagneDiagMont(i, j, joueur)||aGagneDiagDesc(i, j, joueur)) return true;
+			for(int j=0;j<grille[i].length;j++) {
+				if(aGagneHor(i, j, joueur)||aGagneVer(i, j, joueur)||aGagneDiagMont(i, j, joueur)||aGagneDiagDesc(i, j, joueur) )return true;
 			}
 		}
 		return false;
@@ -144,44 +139,40 @@ public class projet {
 		return true;
 	}
 	public static boolean matchNul() {
-		if(verifierPremièreLigne() && !aGagne(1) && !aGagne(2)) return true;
+		if(verifierPremièreLigne() == true && !aGagne(1)== true && !aGagne(2)== true) return true;
 		else return false;
 	}
 	static int joueur=1;
-	public static int phase1() {
-		afficheGrille();
+	public static void boucle() {
 		Scanner sc = new Scanner(System.in);
-		boolean check=false;
-		int c=0;
-		while(!check) {
-			System.out.println("Quel coup pour le joueur "+joueur+" ?");
-			c=sc.nextInt();
-			if(c<0||c>6) {
-				System.out.println("Coup invalide");
+		while((!aGagne(1) && !aGagne(2)) && !matchNul()){
+			afficheGrille();
+			boolean check=false;
+			int c=0;
+			while(!check) {
+				System.out.println("Quel coup pour le joueur "+joueur+" ?");
+				c=sc.nextInt();
+				if(c<0||c>6) {
+					System.out.println("Coup invalide");
+				}
+				else check=true;
 			}
-			else check=true;
+			jouer(joueur, c);
+			joueur=3-joueur;
 		}
 		sc.close();
-		return c;
-	}
-	public static void phase2(int coup) {
-		jouer(joueur, coup);
-		joueur=3-joueur;
-	}
-	public static void boucle() {
-		while(!aGagne(1) && !aGagne(2) && matchNul()){
-			phase2(phase1());
-		}
 	}
 	public static void jeu() {
 		initialisationGrille();
 		boucle();
 		afficheGrille();
-		if(aGagne(1)) System.out.println("Le joueur 1 a gagné !");
-		else if(aGagne(2)) System.out.println("Le joueur 2 a gagné !");
+		if(aGagne(1) == true) System.out.println("le joueur 1 a gagné !");
+		else if(aGagne(2)== true) System.out.println("le joueur 2 a gagné !");
 		else {
-			if(matchNul()) System.out.println("Match nul !");
+			if(matchNul() == true) System.out.println("Match Nul !");
 		}
+	} 
+	public static void main(String[] args){
+		jeu();
 	}
-
 }
